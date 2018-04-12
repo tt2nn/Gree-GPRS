81d7302ad1e0395c9ca35bf759b01adb5ffa1ff1
lihaotian <lihaotian@mirror-networks.com>
2018-04-12 10:53:18 +0800

    add try cache

--- GreeGprs/src/com/gree/gprs/uart/model/MbReadBitModel.java
+++ GreeGprs/src/com/gree/gprs/uart/model/MbReadBitModel.java
@@ -19,32 +19,38 @@ public class MbReadBitModel {
 	 */
 	public static void analyze() {
 
-		if (!Variable.Gprs_Choosed && !DoChoose.isChooseResp()) {
+		try {
 
-			return;
+			if (!Variable.Gprs_Choosed && !DoChoose.isChooseResp()) {
+
+				return;
+			}
+
+			Variable.Uart_Out_Buffer[2] = Variable.Uart_In_Buffer[0];
+			Variable.Uart_Out_Buffer[3] = Variable.Uart_In_Buffer[1];
+
+			// 读 bit长度 转化byte长度
+			int dataLength = Utils.bytesToInt(Variable.Uart_In_Buffer, 4, 5) / 8;
+			Variable.Uart_Out_Buffer[4] = (byte) dataLength;
+
+			// 数据内容
+			for (int i = 5; i < dataLength + 5; i++) {
+
+				Variable.Uart_Out_Buffer[i] = Variable.Server_Data_Short_Buffer[i - 5];
+			}
+
+			// crc16
+			byte[] crc16 = CRC.crc16(Variable.Uart_Out_Buffer, 2, dataLength + 5);
+			Variable.Uart_Out_Buffer[dataLength + 5] = crc16[1];
+			Variable.Uart_Out_Buffer[dataLength + 6] = crc16[0];
+
+			Utils.resetData(Variable.Server_Data_Short_Buffer);
+
+			UartModel.build(dataLength + 7);
+
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-
-		Variable.Uart_Out_Buffer[2] = Variable.Uart_In_Buffer[0];
-		Variable.Uart_Out_Buffer[3] = Variable.Uart_In_Buffer[1];
-
-		// 读 bit长度 转化byte长度
-		int dataLength = Utils.bytesToInt(Variable.Uart_In_Buffer, 4, 5) / 8;
-		Variable.Uart_Out_Buffer[4] = (byte) dataLength;
-
-		// 数据内容
-		for (int i = 5; i < dataLength + 5; i++) {
-
-			Variable.Uart_Out_Buffer[i] = Variable.Server_Data_Short_Buffer[i - 5];
-		}
-
-		// crc16
-		byte[] crc16 = CRC.crc16(Variable.Uart_Out_Buffer, 2, dataLength + 5);
-		Variable.Uart_Out_Buffer[dataLength + 5] = crc16[1];
-		Variable.Uart_Out_Buffer[dataLength + 6] = crc16[0];
-
-		Utils.resetData(Variable.Server_Data_Short_Buffer);
-
-		UartModel.build(dataLength + 7);
 	}
 
 }
--- GreeGprs/src/com/gree/gprs/uart/model/MbReadWordModel.java
+++ GreeGprs/src/com/gree/gprs/uart/model/MbReadWordModel.java
@@ -22,100 +22,108 @@ public class MbReadWordModel {
 	 */
 	public static void analyze() {
 
-		if (!Variable.Gprs_Choosed && !DoChoose.isChooseResp()) {
+		try {
 
-			return;
+			if (!Variable.Gprs_Choosed && !DoChoose.isChooseResp()) {
+
+				return;
+			}
+
+			Variable.Uart_Out_Buffer[2] = Variable.Uart_In_Buffer[0];
+			Variable.Uart_Out_Buffer[3] = Variable.Uart_In_Buffer[1];
+
+			// 获取读数据长度
+			int dataLength = Utils.bytesToInt(Variable.Uart_In_Buffer, 4, 5) * 2;
+			Variable.Uart_Out_Buffer[4] = (byte) dataLength;
+
+			// word0
+			Variable.Uart_Out_Buffer[5] = (byte) 0x00;
+			Variable.Uart_Out_Buffer[6] = Constant.GPRS_MODEL;
+
+			// word1~8
+			byte[] imeiBytes = Device.getInstance().getImei().getBytes();
+			for (int i = 7; i < 7 + imeiBytes.length; i++) {
+
+				Variable.Uart_Out_Buffer[i] = imeiBytes[i - 7];
+			}
+			Variable.Uart_Out_Buffer[22] = (byte) 0x00;
+
+			// word9
+			Variable.Uart_Out_Buffer[23] = (byte) 0x00;
+
+			if (Variable.GPRS_ERROR_TYPE != Constant.GPRS_ERROR_TYPE_NO) {
+
+				Variable.Uart_Out_Buffer[24] = (byte) 0x02;
+
+			} else if (Variable.Transmit_Type == Constant.TRANSMIT_TYPE_STOP) {
+
+				Variable.Uart_Out_Buffer[24] = (byte) 0x00;
+
+			} else {
+
+				Variable.Uart_Out_Buffer[24] = (byte) 0x01;
+			}
+
+			// word10
+			Variable.Uart_Out_Buffer[25] = (byte) 0x00;
+			// 故障代码
+			switch (Variable.GPRS_ERROR_TYPE) {
+
+			case Constant.GPRS_ERROR_TYPE_SIM:
+
+				Variable.Uart_Out_Buffer[26] = (byte) 0x00;
+				break;
+
+			case Constant.GPRS_ERROR_TYPE_NETWORK:
+
+				Variable.Uart_Out_Buffer[26] = (byte) 0x01;
+				break;
+
+			case Constant.GPRS_ERROR_TYPE_SERVER:
+
+				Variable.Uart_Out_Buffer[26] = (byte) 0x03;
+				break;
+
+			default:
+				Variable.Uart_Out_Buffer[26] = (byte) 0x00;
+				break;
+			}
+
+			// word11
+			Variable.Uart_Out_Buffer[27] = (byte) 0x00;
+			Variable.Uart_Out_Buffer[28] = (byte) DeviceConfigure.getNetworkSignalLevel();
+
+			// word12
+			Variable.Uart_Out_Buffer[29] = (byte) 0x00;
+			if (Variable.Server_Data_Change) {
+
+				Variable.Uart_Out_Buffer[30] = (byte) 0xFF;
+				Variable.Server_Data_Change = false;
+
+			} else {
+
+				Variable.Uart_Out_Buffer[30] = (byte) 0x00;
+			}
+
+			int readStart = Utils.bytesToInt(Variable.Uart_In_Buffer, 2, 3);
+
+			// 回复读数据内容
+			for (int i = 31; i < dataLength + 5; i++) {
+
+				Variable.Uart_Out_Buffer[i] = Variable.Server_Data_Long_Buffer[i - 31 + readStart];
+			}
+
+			// crc16校验
+			byte[] crc16 = CRC.crc16(Variable.Uart_Out_Buffer, 2, dataLength + 5);
+			Variable.Uart_Out_Buffer[dataLength + 5] = crc16[1];
+			Variable.Uart_Out_Buffer[dataLength + 6] = crc16[0];
+
+			UartModel.build(dataLength + 7);
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
 		}
-
-		Variable.Uart_Out_Buffer[2] = Variable.Uart_In_Buffer[0];
-		Variable.Uart_Out_Buffer[3] = Variable.Uart_In_Buffer[1];
-
-		// 获取读数据长度
-		int dataLength = Utils.bytesToInt(Variable.Uart_In_Buffer, 4, 5) * 2;
-		Variable.Uart_Out_Buffer[4] = (byte) dataLength;
-
-		// word0
-		Variable.Uart_Out_Buffer[5] = (byte) 0x00;
-		Variable.Uart_Out_Buffer[6] = Constant.GPRS_MODEL;
-
-		// word1~8
-		byte[] imeiBytes = Device.getInstance().getImei().getBytes();
-		for (int i = 7; i < 7 + imeiBytes.length; i++) {
-
-			Variable.Uart_Out_Buffer[i] = imeiBytes[i - 7];
-		}
-		Variable.Uart_Out_Buffer[22] = (byte) 0x00;
-
-		// word9
-		Variable.Uart_Out_Buffer[23] = (byte) 0x00;
-
-		if (Variable.GPRS_ERROR_TYPE != Constant.GPRS_ERROR_TYPE_NO) {
-
-			Variable.Uart_Out_Buffer[24] = (byte) 0x02;
-
-		} else if (Variable.Transmit_Type == Constant.TRANSMIT_TYPE_STOP) {
-
-			Variable.Uart_Out_Buffer[24] = (byte) 0x00;
-
-		} else {
-
-			Variable.Uart_Out_Buffer[24] = (byte) 0x01;
-		}
-
-		// word10
-		Variable.Uart_Out_Buffer[25] = (byte) 0x00;
-		// 故障代码
-		switch (Variable.GPRS_ERROR_TYPE) {
-
-		case Constant.GPRS_ERROR_TYPE_SIM:
-
-			Variable.Uart_Out_Buffer[26] = (byte) 0x00;
-			break;
-
-		case Constant.GPRS_ERROR_TYPE_NETWORK:
-
-			Variable.Uart_Out_Buffer[26] = (byte) 0x01;
-			break;
-
-		case Constant.GPRS_ERROR_TYPE_SERVER:
-
-			Variable.Uart_Out_Buffer[26] = (byte) 0x03;
-			break;
-
-		default:
-			Variable.Uart_Out_Buffer[26] = (byte) 0x00;
-			break;
-		}
-
-		// word11
-		Variable.Uart_Out_Buffer[27] = (byte) 0x00;
-		Variable.Uart_Out_Buffer[28] = (byte) DeviceConfigure.getNetworkSignalLevel();
-
-		// word12
-		Variable.Uart_Out_Buffer[29] = (byte) 0x00;
-		if (Variable.Server_Data_Change) {
-
-			Variable.Uart_Out_Buffer[30] = (byte) 0xFF;
-			Variable.Server_Data_Change = false;
-
-		} else {
-
-			Variable.Uart_Out_Buffer[30] = (byte) 0x00;
-		}
-
-		int readStart = Utils.bytesToInt(Variable.Uart_In_Buffer, 2, 3);
-		// 回复读数据内容
-		for (int i = 31; i < dataLength + 5; i++) {
-
-			Variable.Uart_Out_Buffer[i] = Variable.Server_Data_Long_Buffer[i - 31 + readStart];
-		}
-
-		// crc16校验
-		byte[] crc16 = CRC.crc16(Variable.Uart_Out_Buffer, 2, dataLength + 5);
-		Variable.Uart_Out_Buffer[dataLength + 5] = crc16[1];
-		Variable.Uart_Out_Buffer[dataLength + 6] = crc16[0];
-
-		UartModel.build(dataLength + 7);
 	}
 
 }
