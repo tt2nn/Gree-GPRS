d6e32cff6d060ed483b623a3306cddccab8b789f
lihaotian <lihaotian@mirror-networks.com>
2018-04-18 18:23:05 +0800

    add log

--- GreeGprs/src/com/gree/gprs/Boot.java
+++ GreeGprs/src/com/gree/gprs/Boot.java
@@ -54,10 +54,12 @@ public class Boot {
 		}
 
 		DeviceConfigure.deviceInfo();
+		
+		Logger.logDeviceInfo();
 
 		Apn apn = Utils.getApn();
 		DeviceConfigure.setApn(apn);
-
+		
 		Logger.logApn();
 
 		Variable.Gprs_Mac[0] = Utils.stringToByte(Device.getInstance().getImei().substring(1, 3));
