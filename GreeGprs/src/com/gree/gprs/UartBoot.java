93cbd6a4a73f880dd74014107649d71d5a9b6b8d
lihaotian <lihaotian@mirror-networks.com>
2018-06-22 18:03:03 +0800

    2G revert version

--- GreeGprs/src/com/gree/gprs/UartBoot.java
+++ GreeGprs/src/com/gree/gprs/UartBoot.java
@@ -14,9 +14,9 @@ public class UartBoot extends Boot {
 
 	public static void main(String[] args) {
 
-		Variable.App_Version = "V1.14";
+		Variable.App_Version = "V1.13";
 		Variable.App_Version_First = (byte) 0x01;
-		Variable.App_Version_Second = (byte) 0x0E;
+		Variable.App_Version_Second = (byte) 0x0D;
 
 		ControlCenter.setControlInterface(new UartControlDelegate());
 		DataCenter.setDataInterface(new UartDataDelegate());
