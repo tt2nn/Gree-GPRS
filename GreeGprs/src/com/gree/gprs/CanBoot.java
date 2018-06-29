b27fb76f32760760c2dfc0c9f2351360c65c34b5
lihaotian <lihaotian@mirror-networks.com>
2018-06-29 16:10:12 +0800

    升级版本 修改出厂配置

--- GreeGprs/src/com/gree/gprs/CanBoot.java
+++ GreeGprs/src/com/gree/gprs/CanBoot.java
@@ -11,9 +11,9 @@ public class CanBoot extends Boot {
 
 	public static void main(String[] args) {
 
-		Variable.App_Version = "V1.9";
+		Variable.App_Version = "V1.10";
 		Variable.App_Version_First = (byte) 0x01;
-		Variable.App_Version_Second = (byte) 0x09;
+		Variable.App_Version_Second = (byte) 0x0A;
 
 		Variable.Gprs_Model = (byte) 0x05;
 		Variable.Baud_Rate = 20000;
