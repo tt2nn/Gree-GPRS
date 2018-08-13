330013fa93eb5580ae82f351b3d28f488089ebda
lihaotian <lihaotian@mirror-networks.com>
2018-08-13 14:56:45 +0800

    update version

--- GreeGprs/src/com/gree/gprs/UartBoot.java
+++ GreeGprs/src/com/gree/gprs/UartBoot.java
@@ -14,9 +14,9 @@ public class UartBoot extends Boot {
 
 	public static void main(String[] args) {
 
-		Variable.App_Version = "V1.18";
+		Variable.App_Version = "V1.19";
 		Variable.App_Version_First = (byte) 0x01;
-		Variable.App_Version_Second = (byte) 0x12;
+		Variable.App_Version_Second = (byte) 0x13;
 
 		ControlCenter.setControlInterface(new UartControlDelegate());
 		DataCenter.setDataInterface(new UartDataDelegate());
