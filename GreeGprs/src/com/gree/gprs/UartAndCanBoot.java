677c770312fa50a6505b8fc5772bca2ec6a95ddb
lihaotian <lihaotian@mirror-networks.com>
2019-06-05 11:29:27 +0800

    2gCan升级1.2

--- GreeGprs/src/com/gree/gprs/UartAndCanBoot.java
+++ GreeGprs/src/com/gree/gprs/UartAndCanBoot.java
@@ -18,9 +18,9 @@ public class UartAndCanBoot extends Boot {
 
 		Init_M3_GREENLAND(false);
 
-		Variable.App_Version = "v1.1";
+		Variable.App_Version = "v1.2";
 		Variable.App_Version_First = (byte) 0x01;
-		Variable.App_Version_Second = (byte) 0x01;
+		Variable.App_Version_Second = (byte) 0x02;
 
 		Variable.Gprs_Model = (byte) 0x11;
 
